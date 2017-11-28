65d59307637da321d0b20be844568d3d3e68f2ef
jojonki <junki.ohmura@gmail.com>
2017-11-28 17:26:56 -0500

    add residual input

--- gated_cnn.py
+++ gated_cnn.py
@@ -8,9 +8,19 @@ class GatedCNN(nn.Module):
         In : (N, sentence_len)
         Out: (N, sentence_len, embd_size)
     '''
-    def __init__(self, seq_len, vocab_size, embd_size, n_layers, kernel, out_chs, ans_size):
+    def __init__(self,
+                 seq_len,
+                 vocab_size,
+                 embd_size,
+                 n_layers,
+                 kernel,
+                 out_chs,
+                 res_block_count,
+                 ans_size):
         super(GatedCNN, self).__init__()
-        self.embd_size = embd_size
+        self.res_block_count = res_block_count
+        # self.embd_size = embd_size
+
         self.embedding = nn.Embedding(vocab_size, embd_size)
 
         # nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, ...
@@ -39,13 +49,15 @@ class GatedCNN(nn.Module):
         A = self.conv_0(x) # (bs, Cout, seq_len, 1?)
         B = self.conv_gate_0(x) # (bs, Cout, seq_len, 1?)
         h = A * F.sigmoid(B) # (bs, Cout, seq_len, 1?)
+        res_input = h # TODO this is h1 not h0
 
-        for conv, conv_gate in zip(self.conv, self.conv_gate):
+        for i, (conv, conv_gate) in enumerate(zip(self.conv, self.conv_gate)):
             A = conv(h)
             B = conv_gate(h)
             h = A * F.sigmoid(B) # (bs, Cout, seq_len, 1?)
-
-        # todo residual
+            if i % self.res_block_count == 0: # size of each residual block
+                h += res_input
+                res_input = h
 
         h = h.view(bs, -1) # (bs, Cout*seq_len)
         out = self.fc(h) # (bs, ans_size)
--- train.py
+++ train.py
@@ -9,9 +9,10 @@ from gated_cnn import GatedCNN
 
 seq_len    = 21
 embd_size  = 200
-n_layers   = 5
+n_layers   = 10
 kernel     = (5, embd_size)
 out_chs    = 64
+res_block_count = 5
 batch_size = 128
 
 
@@ -71,7 +72,7 @@ def test(model, data):
     print('Test Acc: {:.2f} % ({}/{})'.format(100 * correct / counter, correct, counter))
     print('Test Loss: {:.4f}'.format(losses/counter))
 
-model = GatedCNN(seq_len, vocab_size, embd_size, n_layers, kernel, out_chs, vocab_size)
+model = GatedCNN(seq_len, vocab_size, embd_size, n_layers, kernel, out_chs, res_block_count, vocab_size)
 if torch.cuda.is_available():
     model.cuda()
 optimizer = torch.optim.Adadelta(model.parameters())
